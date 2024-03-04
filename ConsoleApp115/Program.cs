a30886d18ca6f7cdd0978bc7e490f0f4657a5ca0
ygursu <yasemingrs@gmail.com>
2024-03-04 23:01:42 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp115/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Kaç adet sayı gireceksiniz:");
+int n = Convert.ToInt32(Console.ReadLine());
+double kareToplam = 0;
+int i = 0;
+while (i < n)
+{
+    Console.Write("{0}. sayıyı girin:", i + 1);
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    kareToplam += Math.Pow(sayi, 2);
+    i++;
+}
+Console.WriteLine("Girilen sayıların karelerinin toplamı={0}", kareToplam);
+Console.ReadKey();
